31825278bee5e8e14c754920eae7e4ed6ce2aaf6
Landon Davidson <accounts@landond.tech>
2025-02-01 13:28:40 -0800

    Add: stuff

---
+++ ex9.cc
@@ -0,0 +1,15 @@
+// Copyright 2025 Landon Davidson
+// landond@uw.edu
+
+#include <iostream>
+
+#include "Vector.h"
+
+int main(int argc, char** argv) {
+  vector333::Vector v1(1, 2, 3);
+  vector333::Vector v2(4, 5, 6);
+  std::cout << v1 << "\n";
+  std::cout << v2 << "\n";
+
+
+}
