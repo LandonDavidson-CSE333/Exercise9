7badb010ce26aed2c420d62aaddcb42e8e971ea5
Landon Davidson <accounts@landond.tech>
2025-02-01 14:35:26 -0800

    Add: more test cases

--- ex9.cc
+++ ex9.cc
@@ -12,25 +12,25 @@ int main(int argc, char** argv) {
   vector333::Vector v3;
 
   // Print initial values
-  std::cout << "v1 = " << v1 << "\n";
-  std::cout << "v2 = " << v2 << "\n";
-  std::cout << "v3 = " << v3 << "\n";
+  std::cout << "(1, 2, 3) = " << v1 << "\n";
+  std::cout << "(4, 5, 6) = " << v2 << "\n";
+  std::cout << "(0, 0, 0) = " << v3 << "\n";
 
   // Add + vectors
-  std::cout << "v1 + v2 = " << v1 + v2 << "\n";
-  std::cout << "v2 + v1 = " << v2 + v1 << "\n";
+  std::cout << "(1, 2, 3) + (4, 5, 6) = " << v1 + v2 << "\n";
+  std::cout << "(4, 5, 6) + (1, 2, 3) = " << v2 + v1 << "\n";
 
   // Subtract - vectors
-  std::cout << "v2 - v1 = " << v2 - v1 << "\n";
-  std::cout << "v1 - v2 = " << v1 - v2 << "\n";
+  std::cout << "(4, 5, 6) - (1, 2, 3) = " << v2 - v1 << "\n";
+  std::cout << "(1, 2, 3) - (4, 5, 6) = " << v1 - v2 << "\n";
 
   // Scalar multiplication
-  std::cout << "v1 * 2 = " << v1 * 2 << "\n";
-  std::cout << "5 * v1 = " << 5 * v1 << "\n";
+  std::cout << "(1, 2, 3) * 2 = " << v1 * 2 << "\n";
+  std::cout << "5 * (1, 2, 3) = " << 5 * v1 << "\n";
 
   // Dot product Vector * Vector
-  std::cout << "v1 * v3 = " << v1 * v3 << "\n";
-  std::cout << "v2 * v1 = " << v2 * v1 << "\n";
+  std::cout << "(1, 2, 3) * (0, 0, 0) = " << v1 * v3 << "\n";
+  std::cout << "(4, 5, 6) * (1, 2, 3) = " << v2 * v1 << "\n";
 
   // Copy and initialize new Vectors
   vector333::Vector v4 = v1 + v2;
@@ -38,14 +38,14 @@ int main(int argc, char** argv) {
   v5 = v1 * 4;
 
   // Print initial values
-  std::cout << "v4 = " << v4 << "\n";
-  std::cout << "v5 = " << v5 << "\n";
+  std::cout << "(1, 2, 3) + (4, 5, 6) = " << v4 << "\n";
+  std::cout << "(1, 2, 3) * 4 = " << v5 << "\n";
 
   // Increment += Vectors
   v1 += v5;
-  std::cout << "v1 += v5: " << v1 << "\n";
+  std::cout << "(1, 2, 3) += (4, 8, 12): " << v1 << "\n";
 
   // Decrement -= Vectors
   v2 -= v4;
-  std::cout << "v2 -= v4: " << v2 << "\n";
+  std::cout << "(4, 5, 6) -= (5, 7, 9): " << v2 << "\n";
 }
